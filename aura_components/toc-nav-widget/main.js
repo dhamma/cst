37a995766d53d8085e10fc89677d1ffa167712f9
yapcheahshen <yapcheahshen@gmail.com>
2013-10-10 00:33:43 +0800

    mul att tik parallel display ok

--- aura_components/toc-nav-widget/main.js
+++ aura_components/toc-nav-widget/main.js
@@ -3,22 +3,14 @@ define(['underscore','backbone','text!./template.tmpl','text!../config.json'],
   return {
     type: 'Backbone',
     events: {
-      "click #opentext":"opentext"
-    },
-    opentext:function(e) {
-      var slot=this.model.get("slot");
-      this.sandbox.emit("newreader",{slot:slot});
+
     },
     render:function() {
       this.html(_.template(template,{}) );
     },
-    setslot:function(slot) {
-      this.model.set("slot",slot);
-    },
     model:new Backbone.Model(),
     initialize: function() {
       this.config=JSON.parse(config);
-      this.sandbox.on("setslot",this.setslot,this);
       this.render();
     }
   };
