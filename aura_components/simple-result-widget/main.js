982321f3e397c0b9fd0a55ac0f4026542dea9ef7
yapcheahshen <yapcheahshen@gmail.com>
2013-10-17 00:35:22 +0800

    multiple search result tab ok

--- aura_components/simple-result-widget/main.js
+++ aura_components/simple-result-widget/main.js
@@ -6,16 +6,33 @@ define(['underscore','backbone','text!./text.tmpl','text!../config.json'],
 
     },
     model:new Backbone.Model(),
+    resize:function() {
+      $el=this.$el;
+      var height=$el.parent().height();
+      //check if inside a tab
+      //if (height=$el.height()) height=$el.parent().parent().height();
+      while (height<20) {
+        $el=$el.parent();
+        if (!$el) break;
+        height=$el.height();
+      }
+
+      this.$el.height(height);
+      this.sandbox.emit("resize."+this.groupid,this);
+    },
     init:function(opts) {
       var that=this;
-      var viewid='I'+Math.round((Math.random()*1000000));
-      this.html( _.template(template,{viewid:viewid}));
+      this.groupid='G'+Math.round((Math.random()*1000000));
+      var viewid='V'+Math.round((Math.random()*1000000));
+      this.html( _.template(template,{viewid:viewid,groupid:this.groupid}));
       setTimeout(function(){
         that.sandbox.emit("init."+viewid,opts);
       },100);
+      this.resize();
     },
     initialize: function() {
       this.sandbox.once("init."+this.$el.data('viewid'),this.init,this);
+      this.sandbox.on("resize",this.resize,this);
     }
   };
 });
